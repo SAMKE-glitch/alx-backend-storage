03ce53ff5038d89f0473420de46988330b328569
SAMKE-glitch <sammythemwa@gmail.com>
2023-12-19 01:35:32 +0300

    bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -7,7 +7,7 @@
 -- Context: Write code in SQL is anice level up!
 
 DELIMITER $$;
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
 BEGIN
 	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
 		INSERT INTO projects (name) VALUES (project_name);
