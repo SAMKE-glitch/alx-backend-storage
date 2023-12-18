236e7754607e5b6c986b7f3ceb1bdae9cac65d6f
SAMKE-glitch <sammythemwa@gmail.com>
2023-12-19 00:50:05 +0300

    task o

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- A sql script that creates a table "Users" with
+-- id, email, name
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
