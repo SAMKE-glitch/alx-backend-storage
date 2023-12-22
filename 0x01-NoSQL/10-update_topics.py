d5668534e9d98aa7d82b26775e5d785a5f38eba1
SAMKE-glitch <sammythemwa@gmail.com>
2023-12-23 02:02:40 +0300

    update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -15,5 +15,4 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document"""
-    return mongo_collection.update_many({"name": name},
-                                        {"$set" {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
